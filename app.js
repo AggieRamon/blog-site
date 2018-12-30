5f5b3910f5ef92a53ae5f689c5df091b2b7c70eb
Ramon Ramon <rramon@est-grp.com>
2018-12-29 23:28:46 -0600

    Install mongoose, and update app.js file to include connection to mLab mongoDB

--- app.js
+++ app.js
@@ -2,8 +2,11 @@
 const   express     = require("express"),
         indexRoutes = require("./routes/index"),
         blogRoutes  = require("./routes/blog"),
+        mongoose    = require("mongoose"),
         app         = express(),
         bodyParser  = express.urlencoded({extended: true});
+
+mongoose.connect("mongodb://mongoadmin:Admin123@ds045157.mlab.com:45157/yelpcampramon", {useNewUrlParser: true});
 
 app.set("view engine", "ejs");
 app.use(bodyParser);
@@ -11,7 +14,6 @@ app.use(express.static(__dirname + "/public"));
 
 app.use("/blogs", blogRoutes);
 app.use(indexRoutes);
-
 
 app.listen(3000, "localhost", function(){
     console.log("Server Start Up");
